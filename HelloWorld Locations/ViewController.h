15e0ec66bd24b7010a9543ac0cef560c4220b8ee
Brian Kalski <brian.kalski@solution-stream.com>
2015-07-05 13:09:38 -0400

    Initial Commit

---
+++ HelloWorld Locations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld Locations
+//
+//  Created by Brian Kalski on 7/5/15.
+//  Copyright (c) 2015 BrianKalski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld Locations/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld Locations
+//
+//  Created by Brian Kalski on 7/5/15.
+//  Copyright (c) 2015 BrianKalski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
